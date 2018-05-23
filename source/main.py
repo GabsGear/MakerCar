0f52494ee01233cd83acbbc0324603993dc4ff43
Gabriel Ghellere <gabriel_ghellere@hotmail.com>
2018-05-23 02:25:46 -0300

    test welcome

--- source/main.py
+++ source/main.py
@@ -1,8 +1,16 @@
 import recognition 
+import voice
 
 def main():
 
     test = recognition.Recognition()
-    test.getFromCam()
+    audio = voice.Voice()
+    
+    name = test.getFromCam()
+    if (name != 'Unknown'):
+        audio.sayHello(str(name))
+
+    else:
+        audio.sayWellcome()
 
 main()
