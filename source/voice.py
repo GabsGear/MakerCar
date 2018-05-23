cba40f0c1b888f3b0ad2d98924fe364255d22ea7
Gabriel Ghellere <gabriel_ghellere@hotmail.com>
2018-05-23 02:59:51 -0300

    Update voice.py

--- source/voice.py
+++ source/voice.py
@@ -1,5 +1,9 @@
 import sys
+from os import path
 import pyttsx3
+import pyaudio
+import wave
+import speech_recognition as sr
 
 class Voice():
 
@@ -22,3 +26,43 @@ class Voice():
 		string = ('Hi stranger, I do not know you. Welcome to Maker Space, what is your name?')
 		engine.say(string)
 		engine.runAndWait()
+
+
+class Record():
+
+	def startRecord(self):
+		CHUNK = 1024
+		FORMAT = pyaudio.paInt16
+		CHANNELS = 2
+		RATE = 44100
+		RECORD_SECONDS = 5
+		WAVE_OUTPUT_FILENAME = "/home/gabs/reconhecimento facial/audios/nome.wav"
+
+		p = pyaudio.PyAudio()
+
+		stream = p.open(format=FORMAT,
+						channels=CHANNELS,
+						rate=RATE,
+						input=True,
+						frames_per_buffer=CHUNK)
+
+		print("* recording")
+
+		frames = []
+
+		for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+			data = stream.read(CHUNK)
+			frames.append(data)
+
+		print("* done recording")
+
+		stream.stop_stream()
+		stream.close()
+		p.terminate()
+
+		wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+		wf.setnchannels(CHANNELS)
+		wf.setsampwidth(p.get_sample_size(FORMAT))
+		wf.setframerate(RATE)
+		wf.writeframes(b''.join(frames))
+		wf.close()
